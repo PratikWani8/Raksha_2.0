dbab39507b016b645bab22549a41e14d3cab5c30
Pratik Wani <pwani8306@gmail.com>
2026-04-14 12:41:10 +0530

    feat: initialize Tailwind CSS configuration

---
+++ client/tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#e91e63",
+        softPink: "#fff5f8",
+      },
+    },
+  },
+  plugins: [],
+};
