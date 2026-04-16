eefe4f962d856db9e814ef9fe9f78faba21b15eb
Pratik Wani <pwani8306@gmail.com>
2026-04-16 14:00:52 +0530

    feat: add progress route

---
+++ server/routes/progressRoutes.js
@@ -0,0 +1,9 @@
+import express from "express"
+import { updateProgress, getProgress } from "../controllers/progressController.js"
+
+const router = express.Router()
+
+router.post("/update", updateProgress)
+router.get("/:userId", getProgress)
+
+export default router
